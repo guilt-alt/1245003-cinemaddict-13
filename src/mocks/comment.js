e71a9b29ce66a3566861e5e38d3b08419a15dbc6
Вячеслав Саввиди <60097174+guilt-alt@users.noreply.github.com>
2021-03-06 00:18:36 +0600

    Revert "Module8-Task2 Правки"

---
+++ src/mocks/comment.js
@@ -0,0 +1,26 @@
+import {getRandomItem, generateDate, generateId} from '../utils/common.js';
+import {EMOTIONS} from '../const.js';
+
+const comments = [
+  `Interesting setting and a good cast`,
+  `Booooooooooring`,
+  `Very very old. Meh`,
+  `Almost two hours? Seriously?`
+];
+
+const authors = [
+  `Tim Macoveev`,
+  `John Doe`,
+  `Michael Random`,
+  `Austin Powers`
+];
+
+export const comment = () => {
+  return {
+    id: generateId(),
+    text: getRandomItem(comments),
+    emotion: getRandomItem(EMOTIONS),
+    author: getRandomItem(authors),
+    date: generateDate(),
+  };
+};
